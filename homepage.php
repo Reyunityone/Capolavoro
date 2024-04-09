5d0e11cc89f2dc5c31fe7b6d7aa2d5bf19c60fae
Perz <reymerzari@hotmail.com>
2024-04-09 12:05:37 +0200

    Cursore ed cazzate importate su homepage

--- homepage.php
+++ homepage.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="homepage.css">
     <title>Document</title>
 </head>
 <body>
+    <svg class="trail" viewBox="0 0 400 400">
+        <path d="M 100 100 L 200 200 L 300 100" />
+    </svg>
     <?php
     session_start();
         echo "Ciao ".$_SESSION["nome"]." ".$_SESSION["cognome"];
     ?>
+    <script src="trail.js"></script>
 </body>
 </html>
